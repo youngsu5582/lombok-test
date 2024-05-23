8745547178b36b000e217b0bb9e210e1f8ef68dc
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-05-23 20:55:40 +0900

    feat : 기능 확인용 테스트 코드 구현

--- src/main/java/org/example/lomboktest/domain/Customer.java
+++ src/main/java/org/example/lomboktest/domain/Customer.java
@@ -1,2 +1,31 @@
-package org.example.lomboktest.domain;public class Customer {
+package org.example.lomboktest.domain;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+@EqualsAndHashCode(of = {"firstName", "secondName"})
+@Slf4j
+public class Customer {
+
+    @ToString.Exclude
+    private Long id;
+    private String product;
+    private String firstName;
+    private String secondName;
+
+    @Builder
+    public Customer(final String product, final String firstName, final String secondName) {
+        this.product = product;
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public void introduce() {
+        log.info("로깅 : 자기소개");
+        System.out.println("제 이름은 " + firstName + secondName + " 입니다.");
+    }
 }
--- src/test/java/org/example/lomboktest/LombokTest.java
+++ src/test/java/org/example/lomboktest/LombokTest.java
@@ -1,24 +1,28 @@
-package org.example.lomboktest.domain;
+package org.example.lomboktest;
 
+import org.example.lomboktest.domain.Customer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.*;
 import java.lang.reflect.Modifier;
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.*;
 
 
-@SpringBootTest
-class CustomerTest {
+class LombokTest {
     @Test
     @DisplayName("NoArgsConstructor 의 접근 제한자를 지정할 수 있다.")
-    void some() throws NoSuchMethodException {
-        final var constructor = Customer.class.getDeclaredConstructor();
-        final var modifier = constructor.getModifiers();
+    void some() {
+        try {
+            final var constructor = Customer.class.getDeclaredConstructor();
+            final var modifier = constructor.getModifiers();
 
-        assertThat(Modifier.isProtected(modifier)).isTrue();
+            assertThat(Modifier.isProtected(modifier)).isTrue();
+        } catch (NoSuchMethodException e) {
+            fail("실패");
+        }
+
     }
 
     @Test
@@ -30,7 +34,7 @@ class CustomerTest {
 
     @Test
     @DisplayName("Builder 를 생성자에 지정시, 생성자에 있는 매개변수만 활용해 빌더를 생성한다.")
-    void some2() throws NoSuchMethodException {
+    void some2() {
         final var builderClass = Customer.CustomerBuilder.class;
         assertThatCode(() -> builderClass.getDeclaredMethod("firstName", String.class))
                 .doesNotThrowAnyException();
@@ -44,6 +48,27 @@ class CustomerTest {
         final var customer1 = new Customer("고기", "조", "이썬");
         final var customer2 = new Customer("야채", "조", "이썬");
 
-        assertThat(customer1.hashCode()).isEqualTo(customer2.hashCode());
+        assertThat(customer1).hasSameHashCodeAs(customer2.hashCode());
+    }
+
+    @Test
+    @DisplayName("Slf4j를 통해 로깅 구현체를 받아 사용 가능하다.")
+    void some4() {
+        final String LOG_FILE_PATH = "target/test.log";
+        final var customer1 = new Customer("고기", "조", "이썬");
+
+        customer1.introduce();
+
+        try (final BufferedReader reader = new BufferedReader(new FileReader("target/test.log"))) {
+            final String line = reader.readLine();
+            assertThat(line).contains("로깅");
+
+            final File logFile = new File(LOG_FILE_PATH);
+            if (logFile.exists()) {
+                logFile.delete();
+            }
+        } catch (IOException e) {
+            fail("실패");
+        }
     }
 }
